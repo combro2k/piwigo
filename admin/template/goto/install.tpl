combro2k/piwigo

commit 32a24fefc9aece305d244933ec9e3272978193bc

feature 1020 added: ability to subscribe to Piwigo Announcement Newsletter from Piwigo itself. Admins can perform this action at the end of installation or on administration panel index.
git-svn-id: http://piwigo.org/svn/branches/2.0@3381 68402e56-0260-453c-a942-63ccdbb3a9ee

admin/template/goto/install.tpl
@@ -149,8 +149,17 @@ TD {
 {else}
 <p>
   <br />
-  <input type="button" name="index" value="{'home'|@translate}" onClick="document.location = 'index.php';"/>
-  <input type="button" name="identification" value="{'Identification'|@translate}" onClick="document.location = 'identification.php';"/>
+  <input type="button" name="identification" value="{'Identification'|@translate}" onClick="window.open('identification.php');"/>
+</p>
+
+<div class="infos">
+  <ul>
+    <li>{'Subscribe to Piwigo Announcements Newsletter'|@translate}</li>
+  </ul>
+</div>
+
+<p>
+  <input type="button" name="subscribe" value="{'Subscribe %s'|@translate|@sprintf:$F_ADMIN_EMAIL}" onClick="window.open('{$SUBSCRIBE_BASE_URL}{$F_ADMIN_EMAIL}');"/>
 </p>
 {/if}
 </div> {* content *}

admin/include/functions.php
@@ -2114,4 +2114,13 @@ SELECT '.$conf['user_fields']['username'].'
 
   return $username;
 }
+
+function get_newsletter_subscribe_base_url($language) {
+  $subscribe_domain = 'piwigo.org';
+  if ('fr_FR' == $language) {
+    $subscribe_domain = 'fr.piwigo.org';
+  }
+  
+  return 'http://'.$subscribe_domain.'/announcement/subscribe.php?email=';
+}
 ?>

admin/intro.php
@@ -115,6 +115,15 @@ else if (isset($_GET['action']) and 'phpinfo' == $_GET['action'])
 
 $template->set_filenames(array('intro' => 'intro.tpl'));
 
+if ($conf['show_newsletter_subscription']) {
+  $template->assign(
+    array(
+      'EMAIL' => $user['email'],
+      'SUBSCRIBE_BASE_URL' => get_newsletter_subscribe_base_url($user['language']),
+      )
+    );
+}
+
 $php_current_timestamp = date("Y-m-d H:i:s");
 list($mysql_version, $db_current_timestamp) = mysql_fetch_row(pwg_query('SELECT VERSION(), CURRENT_TIMESTAMP;'));
 

admin/template/goto/intro.tpl
@@ -1,4 +1,17 @@
 {* $Id$ *}
+{known_script id="jquery" src=$ROOT_URL|@cat:"template-common/lib/jquery.packed.js"}
+{known_script id="jquery.dimensions" src=$ROOT_URL|@cat:"template-common/lib/plugins/jquery.dimensions.packed.js"}
+{known_script id="jquery.cluetip" src=$ROOT_URL|@cat:"template-common/lib/plugins/jquery.cluetip.packed.js"}
+
+<script type="text/javascript">
+jQuery().ready(function(){ldelim}
+  jQuery('.cluetip').cluetip({ldelim}
+    width: 300,
+    splitTitle: '|'
+  });
+});
+</script>
+
 <h2>{'title_default'|@translate}</h2>
 <dl style="padding-top: 30px;">
   <dt>{'Piwigo version'|@translate}</dt>
@@ -7,6 +20,9 @@
       <li><a href="{$PHPWG_URL}"  onclick="window.open(this.href, ''); 
           return false;">Piwigo</a> {$PWG_VERSION}</li>
       <li><a href="{$U_CHECK_UPGRADE}">{'Check for upgrade'|@translate}</a></li>
+{if isset($SUBSCRIBE_BASE_URL)}
+      <li><a href="{$SUBSCRIBE_BASE_URL}{$EMAIL}" onclick="window.open(this.href); return false;" class="cluetip" title="{'Piwigo Announcements Newsletter'|@translate}|{'Subscribe to Piwigo Announcements Newsletter'|@translate|htmlspecialchars|nl2br}">{'Subscribe %s to Piwigo Announcements Newsletter'|@translate|@sprintf:$EMAIL}</a></li>
+{/if}
     </ul>
   </dd>
 

include/config_default.inc.php
@@ -657,6 +657,9 @@ $conf['allow_web_services'] = true;
 // Maximum number of images to be returned foreach call to the web service
 $conf['ws_max_images_per_page'] = 500;
 
+// Display a link to subscribe to Piwigo Announcements Newsletter
+$conf['show_newsletter_subscription'] = true;
+
 // +-----------------------------------------------------------------------+
 // | Filter                                                                |
 // +-----------------------------------------------------------------------+

install.php
@@ -421,6 +421,11 @@ else
   {
     array_push($errors, $error_copy);
   }
+
+  $template->assign(
+    'SUBSCRIBE_BASE_URL',
+    get_newsletter_subscribe_base_url($language)
+    );
 }
 if (count($errors) != 0)
 {

language/en_UK/admin.lang.php
@@ -647,4 +647,9 @@ $lang['Bound template'] = 'Bound template';
 $lang['Downloads'] = 'Downloads';
 $lang['Released on'] = 'Released on';
 $lang['Number of downloads'] = 'Number of downloads';
+// --------- Starting below: New or revised $lang ---- from Colibri
+$lang['Piwigo Announcements Newsletter'] = 'Piwigo Announcements Newsletter';
+$lang['Subscribe to Piwigo Announcements Newsletter'] = 'Keep in touch with Piwigo project, subscribe to Piwigo Announcement Newsletter. You will receive emails when a new release is available (sometimes including a security bug fix, it\'s important to know and upgrade) and when major events happen to the project. Only a few emails a year.';
+$lang['Subscribe %s'] = 'Subscribe %s';
+$lang['Subscribe %s to Piwigo Announcements Newsletter'] = 'Subscribe %s to Piwigo Announcements Newsletter';
 ?>

language/fr_FR/admin.lang.php
@@ -646,4 +646,10 @@ $lang['Bound template'] = 'Template rattaché';
 $lang['Downloads'] = 'Téléchargements';
 $lang['Released on'] = 'Ajouté le';
 $lang['Number of downloads'] = 'Nombre de téléchargements';
+// --------- Starting below: New or revised $lang ---- from Colibri
+$lang['Piwigo Announcements Newsletter'] = 'Newsletter Piwigo';
+$lang['Subscribe to Piwigo Announcements Newsletter'] = 'Restez en contact avec le projet Piwigo, abonnez-vous à la newsletter. Vous recevrez un email lorsqu\'une nouvelle version sera disponible (parfois un bug de sécurité est corrigé, il est important d\'être au courant et de mettre à jour) ou qu\'un événement majeur surviendra sur le projet. Seulement quelques messages par an.';
+$lang['Subscribe %s'] = 'Abonner %s';
+$lang['Subscribe %s to Piwigo Announcements Newsletter'] = 'Abonner %s à la newsletter Piwigo';
+
 ?>

commit 190633591f8a149cff64f13a76ad13d0ecbf8713

merge r3765 from branch 2.0 to trunk
bug 895: r3714 had replaced the "open in a new tab" (introduced in r3381 for
feature:1020)) into a "open in the same", but it is important to keep the
window with newsletter subscription links opened.

admin/template/goto/install.tpl
@@ -148,8 +148,8 @@ TD {
 {else}
 <p>
   <br />
-  <input type="button" name="home" value="{'home'|@translate}" onClick="window.location='index.php';"/>
-  <input type="button" name="admin" value="{'admin'|@translate}" onClick="window.location='admin.php';"/>
+  <input type="button" name="home" value="{'home'|@translate}" onClick="window.open('index.php');"/>
+  <input type="button" name="admin" value="{'admin'|@translate}" onClick="window.open('admin.php');"/>
 </p>
 
 <div class="infos">