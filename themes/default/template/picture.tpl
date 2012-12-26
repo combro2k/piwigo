combro2k/piwigo

commit 131c2b100be6e6a672338b9cf08e893bb4d36019

feature 2816: {strip} smarty tag can introduce bugs in javascript + fix picture.tpl
git-svn-id: http://piwigo.org/svn/trunk@19575 68402e56-0260-453c-a942-63ccdbb3a9ee

themes/default/template/picture.tpl
@@ -258,10 +258,11 @@ y.callService(
 						if (e) e.innerHTML = rating.score;
 						e = document.getElementById("ratingCount");
 						if (e) {ldelim}
-							if (rating.count == 1)
+							if (rating.count == 1) {
 								e.innerHTML = "({'%d rate'|@translate|@escape:'javascript'})".replace( "%d", rating.count);
-							else
+							} else {
 								e.innerHTML = "({'%d rates'|@translate|@escape:'javascript'})".replace( "%d", rating.count);
+              }
 						{rdelim}
 					{rdelim}{rdelim} );
 			{/footer_script}

include/smarty/libs/Smarty_Compiler.class.php
@@ -332,7 +332,7 @@ class Smarty_Compiler extends Smarty {
                 /* strip all $text_blocks before the next '/strip' */
                 for ($j = $i + 1; $j < $for_max; $j++) {
                     /* remove leading and trailing whitespaces of each line */
-                    $text_blocks[$j] = preg_replace('![\t ]*[\r\n]+[\t ]*!', '', $text_blocks[$j]);
+                    $text_blocks[$j] = preg_replace('![\t ]*[\r\n]+[\t ]*!', ' ', $text_blocks[$j]);
                     if ($compiled_tags[$j] == '{/strip}') {                       
                         /* remove trailing whitespaces from the last text_block */
                         $text_blocks[$j] = rtrim($text_blocks[$j]);

commit 100e6706b305a3a1d3e10f8f24deae50d583ace6

smarty bug introduced in r19575

themes/default/template/picture.tpl
@@ -258,9 +258,9 @@ y.callService(
 						if (e) e.innerHTML = rating.score;
 						e = document.getElementById("ratingCount");
 						if (e) {ldelim}
-							if (rating.count == 1) {
+							if (rating.count == 1) {ldelim}
 								e.innerHTML = "({'%d rate'|@translate|@escape:'javascript'})".replace( "%d", rating.count);
-							} else {
+							} else {ldelim}
 								e.innerHTML = "({'%d rates'|@translate|@escape:'javascript'})".replace( "%d", rating.count);
               }
 						{rdelim}